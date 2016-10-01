7f93105ecd28e5a229043d42a17f50b3d6957d45
Thorsten Kohfeldt <mailto__qemu-devel@nongnu.org>
2016-10-01 12:09:53 +0200

    vfio: Prepare RTL8168 quirk mode selection
    In function vfio_probe_rtl8168_bar2_quirk()
    - add local variable 'region' caching pointer &vdev->bars[nr].region
    - add local variable 'q_mr_prio' replacing priority constant 1
    - replace constant 2 for allocation of quirk->mem by quirk->nr_mem
    
    Signed-off-by: Thorsten Kohfeldt <mailto__qemu-devel@nongnu.org>

--- hw/vfio/pci-quirks.c
+++ hw/vfio/pci-quirks.c
@@ -931,30 +931,34 @@ static const MemoryRegionOps vfio_rtl_data_quirk = {
 
 static void vfio_probe_rtl8168_bar2_quirk(VFIOPCIDevice *vdev, int nr)
 {
+    VFIORegion *region;
     VFIOQuirk *quirk;
     VFIOrtl8168Quirk *rtl;
+    int q_mr_prio;
 
     if (!vfio_pci_is(vdev, PCI_VENDOR_ID_REALTEK, 0x8168) || nr != 2) {
         return;
     }
+    region = &vdev->bars[nr].region;
 
     quirk = g_malloc0(sizeof(*quirk));
-    quirk->mem = g_new0(MemoryRegion, 2);
     quirk->nr_mem = 2;
+    q_mr_prio = 1;
+    quirk->mem = g_new0(MemoryRegion, quirk->nr_mem);
     quirk->data = rtl = g_malloc0(sizeof(*rtl));
     rtl->vdev = vdev;
 
     memory_region_init_io(&quirk->mem[0], OBJECT(vdev),
                           &vfio_rtl_address_quirk, rtl,
                           "vfio-rtl8168-window-address-quirk", 4);
-    memory_region_add_subregion_overlap(vdev->bars[nr].region.mem,
-                                        0x74, &quirk->mem[0], 1);
+    memory_region_add_subregion_overlap(region->mem, 0x74, &quirk->mem[0],
+                                        q_mr_prio);
 
     memory_region_init_io(&quirk->mem[1], OBJECT(vdev),
                           &vfio_rtl_data_quirk, rtl,
                           "vfio-rtl8168-window-data-quirk", 4);
-    memory_region_add_subregion_overlap(vdev->bars[nr].region.mem,
-                                        0x70, &quirk->mem[1], 1);
+    memory_region_add_subregion_overlap(region->mem, 0x70, &quirk->mem[1],
+                                        q_mr_prio);
 
     QLIST_INSERT_HEAD(&vdev->bars[nr].quirks, quirk, next);
 
